4782157fbad0234bbd1d7e3ad57c7b6c811d9c2f
xendatro <erh101@case.edu>
2025-02-24 09:46:14 -0500

    added readme and requirements

---
+++ README.md
@@ -0,0 +1,11 @@
+Case IDs: pcd42, erh101
+
+
+Description on how to run code below:
+- cd to root folder
+- create venv --> py -m venv env
+- start venv --> env/Scripts/activate
+- install dependencies --> pip install -r requirements.txt
+- run main.py --> py src/main.py
+- two csv files should appear in root folder (evaluation1.csv, evaluation2.csv) that correspond with metrics calculated from the different classifiers of both sets
+
---
+++ src/hyperparam.py
@@ -0,0 +1,174 @@
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import sklearn.neighbors as neighbors
+import sklearn.svm as svm
+import matplotlib.pyplot as plt
+from sklearn.pipeline import make_pipeline
+import sklearn.preprocessing as preprocessing
+import sklearn.neural_network as neural_network
+from sklearn.utils.class_weight import compute_class_weight
+from collections import Counter
+import numpy as np
+import functions
+import sklearn.tree as tree
+import sklearn.naive_bayes as bayes
+import pandas as pd
+from sklearn.model_selection import cross_validate, KFold
+import random
+
+
+# seeding
+SEED = 2
+random.seed(SEED)
+np.random.seed(SEED)
+
+
+def createArrays(file_name):
+    src = f'src/files/{file_name}'
+    f = open(src, "r")
+    xValues = []
+    yValues = []
+    for line in f:
+        arr = line.split("\t")
+        x = []
+        for i in range(0, len(arr)-1):
+            value = arr[i]
+            if value == "Absent":
+                value = 0
+            elif value == "Present":
+                value = 1
+            else:
+                value = float(arr[i])
+            x.append(value)
+        y = int(arr[len(arr)-1])
+        xValues.append(x)
+        yValues.append(y)
+    f.close()
+    return xValues, yValues
+
+"""
+# create arrays to use from the data
+x1, y1 = functions.createArrays("src/files/set1.txt") 
+x2, y2 = functions.createArrays("src/files/set2.txt")
+
+print(compute_class_weight(class_weight="balanced", classes=np.array([0, 1]), y=y1))
+print(compute_class_weight(class_weight="balanced", classes=np.array([0, 1]), y=y2))
+"""
+params_dt1 = {
+    'max_depth': [None, 5, 10, 15],
+    'min_samples_split': [2, 5, 10],
+
+    'min_samples_leaf': [1, 5, 10],
+    'max_leaf_nodes': [None, 5, 10, 15],
+
+    'splitter': ['best', 'random']
+
+}
+params_bayes1 = {
+
+    'var_smoothing': [1e-10, 1e-9, 1e-8]
+
+}
+params_knn1 = {
+    'kneighborsclassifier__n_neighbors': [3, 5, 7, 10],
+    'kneighborsclassifier__weights': ['uniform', 'distance']}
+params_svm1 = {
+
+    'svc__C': [0.01, 0.1, 1, 10],
+
+    'svc__kernel': ['linear', 'rbf']
+}
+params_nn1 = {
+    'mlpclassifier__hidden_layer_sizes': [(200,100), (100,90), (10, 60)],
+
+    'mlpclassifier__alpha': [0.0001, 0.05],
+
+    'mlpclassifier__learning_rate': ['constant', 'adaptive'],
+    'mlpclassifier__solver': ['sgd', 'adam'],
+}
+
+
+def hyperparameter_tune_tree(X:np.ndarray, y:np.ndarray,params_dt:dict)->dict:
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
+    hyp_tree  = GridSearchCV(tree.DecisionTreeClassifier(), params_dt)
+    hyp_tree.fit(X_train, y_train)
+    #results = {'best_estimate':hyp_tree.best_estimator_, 'best_parmas': hyp_tree.best_params_, }
+    return hyp_tree.best_estimator_, hyp_tree.best_params_
+
+def hyperparameter_tune_naive_bayes(X:np.ndarray, y:np.ndarray,params_nb:dict)->dict:
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
+    hype_bayes = GridSearchCV(bayes.GaussianNB(), params_nb)
+    hype_bayes.fit(X_train, y_train)
+    return hype_bayes.best_estimator_,hype_bayes.best_params_
+
+def hyperparameter_tune_knn(X:np.ndarray, y:np.ndarray,params_knn:dict)->dict:
+    X_train, X_test,y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
+    pipeline_knn = make_pipeline(preprocessing.StandardScaler(), neighbors.KNeighborsClassifier())
+    hyp_knn = GridSearchCV(pipeline_knn, params_knn)
+    hyp_knn.fit(X_train, y_train)
+    return hyp_knn.best_estimator_, hyp_knn.best_params_
+
+def hyperparameter_tune_svm(X:np.ndarray, y:np.ndarray,params_svm:dict)->dict:
+    X_train, X_test,y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
+    pipeline_svm = make_pipeline(preprocessing.StandardScaler(), svm.SVC()) 
+    hyp_svm = GridSearchCV(pipeline_svm, params_svm)
+    hyp_svm.fit(X_train, y_train)
+    return hyp_svm.best_estimator_,hyp_svm.best_params_
+
+def hyperparameter_tune_nn(X:np.ndarray, y:np.ndarray, params_nn:dict)->dict:
+    X_train, X_test,y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
+    pipeline_nn = make_pipeline(preprocessing.StandardScaler(), neural_network.MLPClassifier(max_iter=500, early_stopping=True))
+    hyp_nn = GridSearchCV(pipeline_nn, params_nn)
+    hyp_nn.fit(X_train, y_train)
+    return hyp_nn.best_estimator_,hyp_nn.best_params_
+
+def run_everything(X:np.ndarray, y:np.ndarray, params_dt: dict, params_nb: dict, params_knn: dict, params_svm: dict, params_nn: dict):
+    pass
+
+def applyCrossValidation(model, X, y):
+    kf = KFold(n_splits=10, shuffle=False)
+    scores = cross_validate(model, X, y, cv = kf, scoring=['accuracy', 'precision', 'recall', 'f1'])
+    results = {
+
+        'mean accuracy': scores['test_accuracy'].mean(),
+        'mean precision': scores['test_precision'].mean(),
+        'mean recall': scores['test_recall'].mean(),
+        'mean f1 score': scores['test_f1'].mean()
+    }
+    return results
+
+def hyperparamter_tune_save(X, y, output_file_name:str, params_dt = params_dt1, params_bayes = params_bayes1, params_knn = params_knn1, params_svm = params_svm1, params_nn = params_nn1):
+    results = []
+
+    dt_model, dt_params = hyperparameter_tune_tree(X, y, params_dt)
+    dt_cv = applyCrossValidation(dt_model, X, y)
+    results.append({'Model': 'Decision Tree', 'Params': dt_params, **dt_cv})
+
+    bayes_model, bayes_params = hyperparameter_tune_naive_bayes(X, y, params_bayes)
+    bayes_cv = applyCrossValidation(bayes_model, X, y)
+    results.append({'Model': 'Naive Bayes', 'Params': bayes_params, **bayes_cv})
+
+    knn_model, knn_params = hyperparameter_tune_knn(X, y, params_knn)
+    knn_cv = applyCrossValidation(knn_model, X, y)
+    results.append({'Model': 'KNN', 'Params': knn_params, **knn_cv})
+
+    svm_model, svm_params = hyperparameter_tune_svm(X, y, params_svm)
+    svm_cv = applyCrossValidation(svm_model, X, y)
+    results.append({'Model': 'SVM', 'Params': svm_params, **svm_cv})
+
+    nn_model, nn_params = hyperparameter_tune_nn(X, y, params_nn)
+    nn_cv = applyCrossValidation(nn_model, X, y)
+    results.append({'Model': 'NN', 'Params': nn_params, **nn_cv})
+
+    df = pd.DataFrame(results)
+    df.to_csv(output_file_name, index=False)
+
+# X, y= createArrays(src='src/files/set2.txt') #/Users/prestonstuff/Documents/GitHub/project1b/project1_dataset2.txt
+
+
+# hyperparamter_tune_save(X, y)
+
+
+def run(file_name, output_file_name):
+    X, y = createArrays(file_name=file_name)
+    hyperparamter_tune_save(X, y, output_file_name)
